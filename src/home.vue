16f6ac4cbf23b126b43f9e975d583a51906d6512
name <email>
2020-08-18 15:22:47 +0800

    test

--- src/home.vue
+++ src/home.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="home">
     <h1>{{ msg }}</h1>
-    <h2>欢迎 </h2>
+    <h2 v-bind:title="message">欢迎 </h2>
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+
   data () {
     return {
-      msg: '1111111'
+      msg: '1111111',
+      message:'11222222'
     }
   }
 }
