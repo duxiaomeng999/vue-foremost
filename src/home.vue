f3c1e701f841626ce6b4a0645e941a1d5788a940
name <email>
2020-08-18 14:30:43 +0800

    first commit

---
+++ src/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <h1>{{ msg }}</h1>
+    <h2>欢迎 </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: '1111111'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
